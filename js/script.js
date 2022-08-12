47812e6033c78b2ec6f5227e0a9ea30da6959fe5
William <william.teletabs@hotmail.com>
2022-08-11 23:29:33 -0300

    Iniciando função de menu-mobile

--- js/script.js
+++ js/script.js
@@ -1,41 +1,7 @@
-function initScrollSuave(){
- const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+import initAnimacaoScroll from "./modules/anima-scroll.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import outsideClick from "./modules/outside-click.js"
 
-  function goScroll(event){
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
-  const topo = section.offsetTop;
-  
-  window.scrollTo({
-    top: topo,
-    behavior: 'smooth',
-    });
-  }
-
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', goScroll);
-  });
-}
-initScrollSuave();
-
-
-function initAnimacaoScroll(){
-  const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length){
-  
-    const windowMetade = window.innerHeight * 0.6;
-    
-    function animaScroll(){
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-        if(sectionTop < 0){
-          section.classList.add('ativo')
-        }
-      });
-    }
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
-  }
-}
 initAnimacaoScroll();
+initScrollSuave();
+outsideClick();
