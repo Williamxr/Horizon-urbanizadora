d70ddd6debf5a7f1baf0478980aa7149f0051e49
William <william.teletabs@hotmail.com>
2022-08-16 18:01:34 -0300

    Removendo pasta e importando funções do script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initAnimacaoScroll from "./modules/anima-scroll.js";
 import initScrollSuave from "./modules/scroll-suave.js";
-//import outsideClick from "./modules/outside-click.js"
+import outsideClick from "./modules/outsideclick.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initAnimacaoScroll();
 initScrollSuave();
-//outsideClick();
+outsideClick();
+initMenuMobile();
