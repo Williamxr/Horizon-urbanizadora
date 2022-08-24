1779e131211a615c37c91edd4e1e6df006b1ea5e
William <william.teletabs@hotmail.com>
2022-08-23 22:13:17 -0300

    Criando novo módulo para nova função do site

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,5 @@
+export default function initFuncionamento(){
+
+}
+
+const agora = new Date();
--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAnimacaoScroll from "./modules/anima-scroll.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import outsideClick from "./modules/outsideclick.js";
 import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
 
 initAnimacaoScroll();
 initScrollSuave();
 outsideClick();
 initMenuMobile();
+initFuncionamento();
